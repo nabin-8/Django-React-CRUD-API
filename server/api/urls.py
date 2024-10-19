24eef80b38946c595b0a668c895d32537b5cf0ff
nabin-8 <nabinacharya740@gmail.com>
2024-10-19 13:44:08 +0545

    POST API

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_Books
+from .views import get_books, create_books
 
 
 urlpatterns = [
-    path('books/', get_books, name='get_books')
+    path('books/', get_books, name='get_books'),
+    path('books/create/', create_books, name='create_books')
 ]
